cb4b340872368a4b417ec3fd6cd831343803b553
shraddhamali <shraddhamali08@gmail.com>
2020-09-03 12:22:36 +0530

    Initial commit

---
+++ app/src/main/java/com/shraddha/savevideo/MainActivity.java
@@ -0,0 +1,44 @@
+package com.shraddha.savevideo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+
+    VideoView videoView;
+
+    Button button;
+    Intent videoIntent;
+    static final int REQUEST_VIDEO_CAPTURE = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        button=findViewById(R.id.button);
+        videoView=findViewById(R.id.videoView);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent takeVideoIntent = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+                if (takeVideoIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
+                }
+            }
+        });
+
+
+    }
+
+
+}
